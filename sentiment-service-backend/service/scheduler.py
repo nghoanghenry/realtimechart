7fc1c5827704957c03d22d76068c412c297743c8
Phúc Khang <84757707+PhKhang@users.noreply.github.com>
2025-08-07 22:16:14 +0700

    fix: single quotes inside single quotes

--- sentiment-service-backend/service/scheduler.py
+++ sentiment-service-backend/service/scheduler.py
@@ -7,23 +7,28 @@ from service import RSSFetcher, Analyzer
 class FetchScheduler:
     def __init__(self) -> None:
         self.scheduler = BackgroundScheduler()
-        self.scheduler.add_job(self._fetch_job, trigger='interval', minutes=1)
-        self.rss_fetcher = RSSFetcher(['https://www.reddit.com/r/CryptoCurrency.rss', 'https://finance.yahoo.com/news/rssindex'])
+        self.scheduler.add_job(self._fetch_job, trigger="interval", minutes=1)
+        self.rss_fetcher = RSSFetcher(
+            [
+                "https://www.reddit.com/r/CryptoCurrency.rss",
+                "https://finance.yahoo.com/news/rssindex",
+            ]
+        )
         self.analyzer = Analyzer()
 
     def _fetch_job(self) -> None:
         articles = self.rss_fetcher.fetch_rss()
-        print(f'[✔] Found {len(articles)} new articles')
+        print(f"[✔] Found {len(articles)} new articles")
         for article in self.rss_fetcher.fetch_rss():
-            print(f'[✔] Found new entry: {article['link']}')
-            sentiment_result = self.analyzer.analyze(article['content'])
+            print(f"[✔] Found new entry: {article['link']}")
+            sentiment_result = self.analyzer.analyze(article["content"])
             if self.analyzer.check_result(sentiment_result):
-                print(f'[✔] Analyzed sentiment for : {article['link']}')
+                print(f"[✔] Analyzed sentiment for : {article['link']}")
                 doc = {**article, **sentiment_result}
                 save_sentiment(doc)
-                print(f'[✔] Saved result for: {article['link']}')
+                print(f"[✔] Saved result for: {article['link']}")
             else:
-                print(f'[!] Failed to analyze sentiment for : {article['link']}')
+                print(f"[!] Failed to analyze sentiment for : {article['link']}")
 
     def start(self) -> None:
         self.scheduler.start()
